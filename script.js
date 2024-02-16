0914d2ad55f7a5db454989b9957ad512eaeb42e8
kssanabria <kariestef20041998@gmail.com>
2024-02-16 12:38:42 -0500

    Calcular factorial

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
  //parseFloat(prompt("Ingrese un número para calcular el Factorial"))
 
 
-function calcularFibonacci(){
+function calcularFactorial(){
     let number = document.getElementById("number").value;
     number=parseInt(number)
-    let fibonacci= [0,1];
+    let factorial = 1;
     if(number==null || /\D/.test(number) || number== ""){
         Swal.fire({
             title: `Hola, ${number} no es un número, ingresa un número válido`,
             icon: "error",
         })
     }else{
-    for(i=2; i<number; i++){
-        fibonacci[i] = fibonacci [i-1] + fibonacci[i-2];
+    for(i=1; i<=number; i++){
+        factorial *= i;
     }
     Swal.fire({
-        title: `La serie Fibonacci de ${number} es ${fibonacci}`,
+        title: `El factorial de ${number} es ${factorial}`,
         icon: "info",
     })
 }
